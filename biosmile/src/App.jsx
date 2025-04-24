bc1fa03fe529bcd47d9fa663215f0b1744837bc9
unknown <Jernimo>
2025-04-24 19:35:42 -0300

    haré un pull

--- biosmile/src/App.jsx
+++ biosmile/src/App.jsx
@@ -1,10 +1,10 @@
-import NavbarC from "./components/NavbarC";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/navbar" element={<NavbarC />} />
+        <Route />
       </Routes>
     </Router>
   );
